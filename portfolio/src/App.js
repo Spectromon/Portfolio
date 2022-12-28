273fc4ebd622d75ec43323bd516fbdaf08c2aa8b
Spectromon <tommycarter52@gmail.com>
2022-12-28 21:42:21 +0000

    add theme changes and basic nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/nav/Navbar";
 
 function App() {
+  const [theme, setTheme] = useState(window.localStorage.getItem("theme"));
+
+  function changeTheme() {
+    console.log(theme);
+    switch(theme) {
+      case "light":
+        setTheme("dark")
+        break;
+      case "dark":
+        setTheme("light")
+        break;
+      default:
+        setTheme("light")
+        // code block
+    }
+  }
+
+  const ButtonIcon = () =>{
+    switch(theme) {
+      case "light":
+        return(<i className="gg-sun"></i>);
+      case "dark":
+        return(<i className="gg-moon"></i>);
+      default:
+        return(<i className="gg-sun"></i>);
+        // code block
+    }
+  }
+
+  useEffect(() => {
+    if (theme !== "dark" && theme !== "light") {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" data-theme={theme}>
+    <Navbar />
+    <button onClick ={changeTheme}><ButtonIcon/></button>
     </div>
-  );
+  )
 }
 
 export default App;
